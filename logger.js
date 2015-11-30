dc26a61f0d715e4da1b7249f2f12805b4a1cc463
Deepankar Arya <deepankar1994@gmail.com>
2015-11-30 12:40:20 +0530

    Mouse events added

--- logger.js
+++ logger.js
@@ -4,14 +4,35 @@ $(document).ready(function(){
 
 
 
-
+//Handler for key press events
   document.onkeydown = function(evt) {
     evt = evt || window.event;
   
-    	console.log(evt.which);
+    	console.log(String.fromCharCode(evt.which));
    
 };
 
+
+// Handler for mouse click events
+$('html').mousedown(function(event) {
+
+	var mouseEvent;
+    switch (event.which) {
+        case 1:
+            mouseEvent='Left Click';
+            break;
+        case 2:
+            mouseEvent='Middle Click';
+            break;
+        case 3:
+            mouseEvent='Right Click';
+            break;
+        default:
+            mouseEvent='Unknown Click';
+    }
+
+    console.log(mouseEvent);
+});
 
     
 });
